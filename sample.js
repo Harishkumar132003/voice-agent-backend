6a01f907a3b8d86ef242bcaa3a43d673ebf1ab1c
HarishKumar133 <harish.a@bluefunda.com>
2025-08-08 17:00:04 +0530

    edit docker

--- sample.js
+++ sample.js
@@ -4,28 +4,33 @@ const cors = require("cors");
 
 const { AccessToken,AgentDispatchClient ,RoomServiceClient} = require("livekit-server-sdk");
 
-async function createExplicitDispatch(roomName,id,skill,name) {
-  const agentName = 'interview-agent'
-  const agentDispatchClient = new AgentDispatchClient(process.env.LIVEKIT_HTTP, process.env.LIVEKIT_API_KEY, process.env.LIVEKIT_API_SECRET);
+// async function createExplicitDispatch(roomName,id,skill,name) {
+//   const agentName = 'interview-agent'
+//   const agentDispatchClient = new AgentDispatchClient(process.env.LIVEKIT_HTTP, process.env.LIVEKIT_API_KEY, process.env.LIVEKIT_API_SECRET);
 
-  // create a dispatch request for an agent named "test-agent" to join "my-room"
-  const dispatch = await agentDispatchClient.createDispatch(roomName,agentName,{
-    metadata: JSON.stringify({
-    id,
-    skills: skill.split(','),
-    name:name
-  }),
-  });
-  console.log('created dispatch', dispatch);
+//   // create a dispatch request for an agent named "test-agent" to join "my-room"
+//   const dispatch = await agentDispatchClient.createDispatch(roomName,agentName,{
+//     metadata: JSON.stringify({
+//     id,
+//     skills: skill.split(','),
+//     name:name
+//   }),
+//   });
+//   console.log('created dispatch', dispatch);
 
-  // const dispatches = await agentDispatchClient.listDispatch(roomName);
-  // console.log(`there are ${dispatches.length} dispatches in ${roomName}`);
-}
+//   // const dispatches = await agentDispatchClient.listDispatch(roomName);
+//   // console.log(`there are ${dispatches.length} dispatches in ${roomName}`);
+// }
 
 const createToken = async (participantName,roomName,id,skill) => {
   const at = new AccessToken(process.env.LIVEKIT_API_KEY, process.env.LIVEKIT_API_SECRET, {
     identity: participantName,
     ttl: '10m',
+    metadata:JSON.stringify({
+    id,
+    skills: skill.split(','),
+    name:participantName
+  }),
   });
   at.addGrant({
     roomJoin: true,
@@ -34,7 +39,7 @@ const createToken = async (participantName,roomName,id,skill) => {
     canSubscribe: true,
     canPublishData: true,
   });
-createExplicitDispatch(roomName,id,skill,participantName)
+// createExplicitDispatch(roomName,id,skill,participantName)
   return await at.toJwt();
 };
 
